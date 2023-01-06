243757a3b3c9360e6cd42f139a802314c02e4207
Ka Long Ngai <kls8162@gmail.com>
2023-01-06 22:58:21 +0800

    Time Class Update 1 (Formatting).

--- Time.java
+++ Time.java
@@ -2,12 +2,12 @@ public class Time {
     private int hours, minutes, seconds;
 
     public Time(int seconds) {
-        setTime(0,0,seconds);
+        setTime(0, 0, seconds);
         updateTimeMaxHours();
     }
 
     public Time(int minutes, int seconds) {
-        setTime(0,minutes,seconds);
+        setTime(0, minutes, seconds);
         updateTimeMaxHours();
 
     }
@@ -22,7 +22,7 @@ public class Time {
     }
 
     public void setHours(int hours) {
-        if(isUnitOfTimeNegative(hours)) {
+        if (isUnitOfTimeNegative(hours)) {
             throw new IllegalArgumentException("Hours cannot be negative");
         }
         this.hours = hours;
@@ -33,7 +33,7 @@ public class Time {
     }
 
     public void setMinutes(int minutes) {
-        if(isUnitOfTimeNegative(minutes)) {
+        if (isUnitOfTimeNegative(minutes)) {
             throw new IllegalArgumentException("Minutes cannot be negative");
         }
         this.minutes = minutes;
@@ -44,7 +44,7 @@ public class Time {
     }
 
     public void setSeconds(int seconds) {
-        if(isUnitOfTimeNegative(seconds)) {
+        if (isUnitOfTimeNegative(seconds)) {
             throw new IllegalArgumentException("Seconds cannot be negative");
         }
         this.seconds = seconds;
@@ -63,40 +63,49 @@ public class Time {
     public static int[] timeConvert(int smallerUnitOfTime) {
         int biggerUnitOfTime = smallerUnitOfTime / 60;
         smallerUnitOfTime = smallerUnitOfTime % 60;
-        return new int[] {smallerUnitOfTime, biggerUnitOfTime};
+        return new int[] { smallerUnitOfTime, biggerUnitOfTime };
     }
 
     private void updateTimeMaxHours() {
-        if(seconds < 60) {
-            return;
-        }
         int minutesSecondsArr[] = timeConvert(seconds);
         seconds = minutesSecondsArr[0];
-        if(minutesSecondsArr[1] < 60) {
-            minutes+=minutesSecondsArr[1];
-            return; 
-        }
-        int hoursMinutesArr[] = timeConvert(minutesSecondsArr[1]);
+        minutes += minutesSecondsArr[1];
+        int hoursMinutesArr[] = timeConvert(minutes);
         minutes = hoursMinutesArr[0];
-        hours+=hoursMinutesArr[1];
+        hours += hoursMinutesArr[1];
     }
 
     public String toString() {
         return "" + hours + " hours " + minutes + " minutes " + seconds + " seconds.";
     }
-    
 
-    //TestCode
+    // TestCode
     public static void main(String[] args) {
         Time secondsTestOne = new Time(30);
         Time secondsTestTwo = new Time(119);
         Time secondsTestThree = new Time(3661);
+        Time overAllTestOne = new Time(1000, 93);
+        Time overAllTestTwo = new Time(199, 129102, 23);
+        Time overAllTestThree = new Time(1, 120, 33);
         System.out.println(secondsTestOne + "\n" + secondsTestTwo + "\n" + secondsTestThree);
+        System.out.println(overAllTestOne + " \n" + overAllTestTwo + "\n" + overAllTestThree);
         try {
-            Time secondsTestFour = new Time(-1);
-            System.out.println(secondsTestFour);
+            Time secondsExceptionTest = new Time(-1);
+            System.out.println(secondsExceptionTest);
         } catch (IllegalArgumentException ex) {
             System.out.println("Seconds Exception Passed");
         }
+        try {
+            Time minutesExceptionTest = new Time(-1, 0);
+            System.out.println(minutesExceptionTest);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("minutes Exception Passed");
+        }
+        try {
+            Time hoursExceptionTest = new Time(-1, 0);
+            System.out.println(hoursExceptionTest);
+        } catch (IllegalArgumentException ex) {
+            System.out.println("hours Exception Passed");
+        }
     }
-}  
+}
