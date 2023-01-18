e85c12ba09e7f10b8fca0c40bb32460a38f0f272
Ka Long Ngai <kls8162@gmail.com>
2023-01-17 20:05:05 -0500

    Album Class extend EP class.

--- Album.java
+++ Album.java
@@ -1,3 +1,6 @@
-public class Album {
-    
+public class Album extends ExtendedPlay {
+
+    public Album(String name, Song[] songs, Time lengthOfSongs, Artist artist) {
+        super(name, songs, lengthOfSongs, artist);
+    }
 }
