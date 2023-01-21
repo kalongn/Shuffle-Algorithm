94dee5d290ac7a0caed4f26fcb76021a7bef25ab
Ka Long Ngai <kls8162@gmail.com>
2023-01-21 16:36:10 -0500

    Playlist class basic done.

--- Playlist.java
+++ Playlist.java
@@ -1,3 +1,32 @@
-public class Playlist {
+import java.util.ArrayList;
+public class Playlist extends ArrayList<Song> {
     
+    public void enqueue(Song song) {
+        super.add(song);
+    }
+
+    public Song dequeue() {
+        return super.remove(0);
+    }
+
+    public void song(int index1, int index2) throws IndexOutOfBoundsException{
+        if(index1 > this.size()-1 || index2 > this.size() -1 ) {
+            throw new IndexOutOfBoundsException();
+        }
+        Song temp = this.get(index1);
+        this.set(index1, this.get(index2));
+        this.set(index2, this.get(index1));
+    }
+
+    public void absoluteShuffle() {
+
+    }
+
+    public void songCollectionShuffle() {
+
+    }
+
+    public void similaritySongShuffle() {
+
+    }
 }
