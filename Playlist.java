a9f812ec2f2482f97748656a66cce62ff8e10389
Ka Long Ngai <kls8162@gmail.com>
2023-01-22 20:17:15 -0500

    Absolute and songCollection shuffle.

--- Playlist.java
+++ Playlist.java
@@ -1,6 +1,10 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Playlist extends ArrayList<Song> {
-    
+
     public void enqueue(Song song) {
         super.add(song);
     }
@@ -9,8 +13,14 @@ public class Playlist extends ArrayList<Song> {
         return super.remove(0);
     }
 
-    public void song(int index1, int index2) throws IndexOutOfBoundsException{
-        if(index1 > this.size()-1 || index2 > this.size() -1 ) {
+    public void addAllFromSongCollection(SongCollection collection) {
+        for (Song song : collection.getSongs()) {
+            super.add(song);
+        }
+    }
+
+    public void swapSong(int index1, int index2) throws IndexOutOfBoundsException {
+        if (index1 > this.size() - 1 || index2 > this.size() - 1) {
             throw new IndexOutOfBoundsException();
         }
         Song temp = this.get(index1);
@@ -18,12 +28,35 @@ public class Playlist extends ArrayList<Song> {
         this.set(index2, temp);
     }
 
-    public void absoluteShuffle() {
+    private int randomInt(int min, int max) {
+        return min + (int) (Math.random() * ((max - min) + 1));
+    }
 
+    public void absoluteShuffle() {
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        for (int i = this.size() - 1; i > 0; i--) {
+            int rndIndex = rnd.nextInt(i + 1);
+            swapSong(rndIndex, i);
+        }
     }
 
     public void songCollectionShuffle() {
-
+        HashSet<SongCollection> allSongCollectionsHS = new HashSet<SongCollection>();
+        for (int i = 0; i < this.size(); i++) {
+            allSongCollectionsHS.add(this.get(i).getSongCollection());
+        }
+        ArrayList<SongCollection> allSongCollectionsAL = new ArrayList<>(allSongCollectionsHS);
+        ThreadLocalRandom rnd = ThreadLocalRandom.current();
+        for (int i = allSongCollectionsAL.size() - 1; i > 0; i--) {
+            int rndIndex = rnd.nextInt(i + 1);
+            SongCollection temp = allSongCollectionsAL.get(rndIndex);
+            allSongCollectionsAL.set(rndIndex, allSongCollectionsAL.get(i));
+            allSongCollectionsAL.set(i, temp);
+        }
+        removeAll(this);
+        for (SongCollection indSongCollections : allSongCollectionsAL) {
+            addAllFromSongCollection(indSongCollections);
+        }
     }
 
     public void similaritySongShuffle() {
