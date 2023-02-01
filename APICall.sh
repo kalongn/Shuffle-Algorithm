5f76583315adb8468e5b0d241ab0ccd2a5c68568
Ka Long Ngai <kls8162@gmail.com>
2023-02-01 15:30:10 -0500

    Artist Stat now collected for each song.

--- APICall.sh
+++ APICall.sh
@@ -19,18 +19,17 @@ echo "" >SongDatas/"$nameOfTrack".txt
 echo 'Track Name: '$nameOfTrack >>"SongDatas/$nameOfTrack.txt"
 
 #Add the Track ID into the Song file
-echo 'Track ID: '$(jq .id SongTempDatas/Song$typeOfAnalysis.JSON) >>"SongDatas/$nameOfTrack.txt"
+echo 'Track ID: '$(jq .id "SongTempDatas/Song$typeOfAnalysis.JSON") >>"SongDatas/$nameOfTrack.txt"
 
 #Add all the artists name, ID into this Song file.
-artistsLength=$(jq '.artists | length' SongTempDatas/Song$typeOfAnalysis.JSON)
+artistsLength=$(jq '.artists | length' "SongTempDatas/Song$typeOfAnalysis.JSON")
 for ((i = 1; i <= $artistsLength; i++)); do
   indexArr=$i-1
   #Add Artist ID / Name into Song File
-  artistName=$(jq .artists[$indexArr].name SongTempDatas/Song$typeOfAnalysis.JSON)
-  artistID=$(jq .artists[$indexArr].id SongTempDatas/Song$typeOfAnalysis.JSON)
+  artistName=$(jq .artists[$indexArr].name "SongTempDatas/Song$typeOfAnalysis.JSON")
+  artistID=$(jq .artists[$indexArr].id "SongTempDatas/Song$typeOfAnalysis.JSON")
   echo 'Artist '$i' Name: '$artistName >>"SongDatas/$nameOfTrack.txt"
   echo 'Artist '$i' Id: '$artistID >>"SongDatas/$nameOfTrack.txt"
-
   #Create Artist File respective to this song.
   echo '' >"ArtistsTempDatas/$artistName.JSON"
   artistID="${artistID:1:-1}"
@@ -41,6 +40,23 @@ for ((i = 1; i <= $artistsLength; i++)); do
     --header 'Authorization: Bearer '$Token \
     --header 'Content-Type: application/json' >"ArtistsTempDatas/$artistName.JSON"
   #
+
+  #Create the actual data file via a txt file.
+  echo '' >"ArtistsDatas/$artistName.txt"
+  #Add artist name, genres and stats into a this new txt file.
+  echo 'Artist Name: '$artistName >>"ArtistsDatas/$artistName.txt"
+  echo 'Artist Popularity: '$(jq .popularity "ArtistsTempDatas/$artistName.JSON") >>"ArtistsDatas/$artistName.txt"
+  echo 'Artist total followers:'$(jq .followers.total "ArtistsTempDatas/$artistName.JSON") >>"ArtistsDatas/$artistName.txt"
+  genresLength=$(jq '.genres | length' "ArtistsTempDatas/$artistName.JSON")
+  allGenres=''
+  for ((z = 0; z < $genresLength; z++)); do 
+    if [ $z == $((genresLength-1)) ]; then
+      allGenres="$allGenres"$(jq .genres[$z] "ArtistsTempDatas/$artistName.JSON")
+    else
+      allGenres="$allGenres"$(jq .genres[$z] "ArtistsTempDatas/$artistName.JSON")" | "
+    fi
+  done
+  echo 'Artist genres: '$allGenres >>"ArtistsDatas/$artistName.txt"
 done
 
 #Add the tracks popularity into the Song file.
