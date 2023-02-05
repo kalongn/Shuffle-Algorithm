eb49f03ff0f399d63204405e7ea892d114c78054
Ka Long Ngai <kls8162@gmail.com>
2023-02-05 16:31:01 -0500

    removed unused Script.

--- RunProgram.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-getPlayListIDFromURL() {
-  #remove everything minus the 32 characters playlist ID.
-  echo "${1:34:-20}"
-}
-
-playlist_id=$(getPlayListIDFromURL $1)
-bash "./APICall.sh" $playlist_id
