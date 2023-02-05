8f0214afca01e2f5c69e63b59e3ee71780f4b48a
Ka Long Ngai <kls8162@gmail.com>
2023-02-05 16:30:32 -0500

    Class basic done.

--- Java_Code_Alpha/MusicController.java
+++ Java_Code_Alpha/MusicController.java
@@ -1,9 +1,28 @@
 package Java_Code_Alpha;
-
+import java.io.IOException;
+import java.util.Scanner;
 public class MusicController {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Spotify playlist URL: ");
+        String playlistID = scanner.nextLine();
+        scanner.close();
+        if(playlistID.length() != 76 || !playlistID.startsWith("https://open.spotify.com/playlist/")) {
+            System.out.println("Invalid URL.");
+            return;
+        }
+        playlistID = playlistID.substring(34,56);
+        try {
+            Runtime.getRuntime().exec(new String[] {"bash", "./APICall.sh", playlistID}).waitFor();
+        } catch (InterruptedException e) {
+            System.out.println("Thread interrupted.");
+            return;
+        } catch (IOException e) {
+            System.out.println("Invalid URL.");
+            e.printStackTrace();
+        }
+        Playlist playlist = Playlist.readFromPlaylistTxt(playlistID);
+        System.out.print(playlist);
+    }
     
-    private Playlist musicPlayer;
-    
-    
-
 }
