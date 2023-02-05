86a3a36bc47a8de8a8d453cb28e67bf54eb2a5f8
Ka Long Ngai <kls8162@gmail.com>
2023-02-05 14:29:57 -0500

    Updated to fit Artists Name into this class.

--- Java_Code_Alpha/Song.java
+++ Java_Code_Alpha/Song.java
@@ -7,13 +7,16 @@ import java.util.Scanner;
 
 public class Song {
     private String trackName;
+    private String[] artistsName;
     private int popularity;
     private double bpm, valence, energy, danceability, acousticness;
     private HashSet<String> genres;
 
-    public Song(String trackName, int popularity, double bpm, double valence, double energy, double danceability,
+    public Song(String trackName, String[] artistsName, int popularity, double bpm, double valence, double energy,
+            double danceability,
             double acousticness, HashSet<String> genres) {
         this.trackName = trackName;
+        this.artistsName = artistsName;
         this.popularity = popularity;
         this.bpm = bpm;
         this.valence = valence;
@@ -38,6 +41,13 @@ public class Song {
         }
 
         String trackName = scanner.nextLine();
+        String artists = scanner.nextLine();
+        String[] artistsName;
+        if (artists.contains(",")) {
+            artistsName = artists.split(",");
+        } else {
+            artistsName = new String[] { artists };
+        }
         int popularity = scanner.nextInt();
         double bpm = scanner.nextDouble();
         double valence = scanner.nextDouble();
@@ -51,11 +61,15 @@ public class Song {
         }
         scanner.close();
 
-        return new Song(trackName, popularity, bpm, valence, energy, danceability, acousticness, genres);
+        return new Song(trackName, artistsName, popularity, bpm, valence, energy, danceability, acousticness, genres);
     }
 
     public String getTrackName() {
         return trackName;
+    }
+
+    public String[] getArtistsName() {
+        return this.artistsName;
     }
 
     public int getPopularity() {
@@ -89,17 +103,26 @@ public class Song {
     @Override
     public String toString() {
         String returnStr = "-------------------------------------------------------------\n" + this.getTrackName();
-        returnStr += "\nThe popularity of this song is " + this.getPopularity() + " out of 100";
-        returnStr += "\nThe BPM of this song is " + this.getBpm();
-        returnStr += "\nThe valence of this song is " + this.getValence() + " out of 1";
-        returnStr += "\nThe energy of this song is " + this.getEnergy() + " out of 1";
-        returnStr += "\nThe danceability of this song is " + this.getDanceability() + " out of 1";
-        returnStr += "\nThe acousticness of this song is " + this.getAcousticness() + " out of 1";
+        returnStr += "\nThe artist(s) of this song is/are: ";
+        for (String i : this.getArtistsName()) {
+            returnStr += "\n- " + i;
+        }
+        returnStr += "\nThe popularity of this song is " + this.getPopularity() + " out of 100.";
+        returnStr += "\nThe BPM of this song is " + this.getBpm() + ".";
+        returnStr += "\nThe valence of this song is " + this.getValence() + " out of 1.";
+        returnStr += "\nThe energy of this song is " + this.getEnergy() + " out of 1.";
+        returnStr += "\nThe danceability of this song is " + this.getDanceability() + " out of 1.";
+        returnStr += "\nThe acousticness of this song is " + this.getAcousticness() + " out of 1.";
         returnStr += "\nThis song may include the following genres: ";
         for (String i : this.getGenres()) {
-            returnStr += "\n" + i;
+            returnStr += "\n- " + i;
         }
-        returnStr += "\n-------------------------------------------------------------";
+        returnStr += ".\n-------------------------------------------------------------";
         return returnStr;
+    }
+
+    public static void main(String[] args) {
+        Song test = readFromSongTxt("0O6u0VJ46W86TxN9wgyqDj");
+        System.out.println(test);
     }
 }
