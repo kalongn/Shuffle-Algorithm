ca9180b6c1aa0014e23dc7213c705c16960ee863
Ka Long Ngai <kls8162@gmail.com>
2023-02-06 11:23:26 -0500

    javadoc Playlist class.

--- Java_Code_Alpha/Playlist.java
+++ Java_Code_Alpha/Playlist.java
@@ -5,15 +5,41 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+/**
+ * This Playlist Class allowing us to create a Playlist object. This class
+ * extended ArrayList while having an additional field variable, aka the
+ * playlist name.
+ * 
+ * @author Ka_Long_Ngai 02/06/2023
+ */
 public class Playlist extends ArrayList<Song> {
 
     private String playlistTitle;
 
+    /**
+     * Create a Playlist object, with an additional playlistTitle field variable.
+     * 
+     * @param playlistTitle
+     *                      The name of this playlist, String.
+     */
     public Playlist(String playlistTitle) {
         this.playlistTitle = playlistTitle;
     }
 
-    public static Playlist readFromPlaylistTxt(String playListID) {
+    /**
+     * read from a PlaylistID file and convert every songID from the playlist into a
+     * Song object and added this new Playlist Object and return it.
+     * 
+     * @param playListID
+     *                   the playlist ID of a spotify URL (32 characters).
+     * @return
+     *         A playlist Object with every valid Song ID which created Song objects
+     *         added into this Playlist.
+     * @exception IllegalArgumentException
+     *                                     thrown when the playList ID is not valid
+     *                                     or file is not found.
+     */
+    public static Playlist readFromPlaylistTxt(String playListID) throws IllegalArgumentException {
         File playListFile = new File("./Condensed_Datas/PlaylistDatas/" + playListID + ".txt");
         Scanner scanner;
         try {
@@ -31,10 +57,19 @@ public class Playlist extends ArrayList<Song> {
         return returnPlaylist;
     }
 
+    /**
+     * @return
+     *         the title/name of this playlist.
+     */
     public String getPlaylistTitle() {
         return this.playlistTitle;
     }
 
+    /**
+     * return the entire playlist in a list format String.
+     * 
+     * @see java.util.AbstractCollection#toString()
+     */
     @Override
     public String toString() {
         String returnString = "-------------------------------------------------------------\n"
