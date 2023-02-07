db2add0e8188e6c20d69c8050ac9e35f079e2cab
Ka Long Ngai <kls8162@gmail.com>
2023-02-07 10:41:32 -0500

    MusicController javadoc.

--- Java_Code_Alpha/MusicController.java
+++ Java_Code_Alpha/MusicController.java
@@ -3,6 +3,15 @@ package Java_Code_Alpha;
 import java.io.IOException;
 import java.util.Scanner;
 
+/**
+ * This MusicController Class allowing us to control a Playlist by directly
+ * giving a Spotify playlist URL. This will call the APICall.sh which get the
+ * required data from the spotify API and then construct all the required data
+ * in java and return a new Playlist object.
+ * 
+ * @author Ka_Long_Ngai 02/07/2023
+ */
+
 public class MusicController {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
