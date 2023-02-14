a1908ce700274f13fe3c2ef066271edf19f3d514
Ka Long Ngai <kls8162@gmail.com>
2023-02-14 09:55:08 -0500

    Serializable implemented.

--- Java_Code_Alpha/MusicController.java
+++ Java_Code_Alpha/MusicController.java
@@ -1,5 +1,9 @@
 package Java_Code_Alpha;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -13,27 +17,46 @@ import java.util.Scanner;
  */
 
 public class MusicController {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter Spotify playlist URL: ");
-        String playlistID = scanner.nextLine();
-        scanner.close();
-        if (playlistID.length() != 76 || !playlistID.startsWith("https://open.spotify.com/playlist/")) {
-            System.out.println("Invalid URL.");
-            return;
-        }
-        playlistID = playlistID.substring(34, 56);
+
+    private Playlist activePlaylist;
+
+    public static void main(String[] args) throws IOException{
+
+        MusicController spotcloud = new MusicController();
+
         try {
-            Runtime.getRuntime().exec(new String[] { "bash", "./APICall.sh", playlistID }).waitFor();
-        } catch (InterruptedException e) {
-            System.out.println("Thread interrupted.");
-            return;
-        } catch (IOException e) {
-            System.out.println("Invalid URL.");
-            e.printStackTrace();
+            FileInputStream file = new FileInputStream("playlist.obj");
+            ObjectInputStream inStream = new ObjectInputStream(file);
+            spotcloud.activePlaylist = (Playlist) inStream.readObject();
+            inStream.close();
+            System.out.println("Detected previous Playlist.");
+        } catch (Exception ex) {
+            System.out.println("no previous playlist detected.\nPlease create a new playlist.");
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Enter Spotify playlist URL: ");
+            String playlistID = scanner.nextLine();
+            scanner.close();
+            if (playlistID.length() != 76 || !playlistID.startsWith("https://open.spotify.com/playlist/")) {
+                System.out.println("Invalid URL.");
+                return;
+            }
+            playlistID = playlistID.substring(34, 56);
+            try {
+                Runtime.getRuntime().exec(new String[] { "bash", "./APICall.sh", playlistID }).waitFor();
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted.");
+                return;
+            } catch (IOException e) {
+                System.out.println("Invalid URL.");
+                e.printStackTrace();
+            }
+            spotcloud.activePlaylist = Playlist.readFromPlaylistTxt(playlistID);
         }
-        Playlist playlist = Playlist.readFromPlaylistTxt(playlistID);
-        System.out.print(playlist);
+        System.out.print(spotcloud.activePlaylist);
+        FileOutputStream file = new FileOutputStream("playlist.obj");
+        ObjectOutputStream outStream = new ObjectOutputStream(file);
+        outStream.writeObject(spotcloud.activePlaylist);
+        outStream.close();
     }
 
 }
