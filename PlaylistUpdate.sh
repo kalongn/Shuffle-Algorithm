7aab4a1a6030451167a6f4c0205ae6dbc7305d34
Ka Long Ngai <kls8162@gmail.com>
2023-03-09 18:40:44 -0500

    Bash script now read all urls line.

--- PlaylistUpdate.sh
+++ PlaylistUpdate.sh
@@ -3,3 +3,16 @@
 returnToken() {
     node fetchToken.js
 }
+
+#program begin here
+filename="shuffleResultPlaylist.txt"
+read -r playlist_id<$filename
+firstLine=false
+while read line; do
+    if $firstLine ; then
+        echo $line
+    fi
+    firstLine=true
+done < $filename
+
+exit 0
