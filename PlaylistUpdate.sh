b38b1158cd0b43aa4471f48a116a595aff9041ec
Ka Long Ngai <kls8162@gmail.com>
2023-03-09 19:14:00 -0500

    Code now succesfully call API.

--- PlaylistUpdate.sh
+++ PlaylistUpdate.sh
@@ -4,13 +4,30 @@ returnToken() {
     node fetchToken.js
 }
 
+updatePlayList() {
+    token=$1
+    playlist_id=$2
+    uris=$3
+
+    URL="https://api.spotify.com/v1/playlists/"$playlist_id"/tracks?uris=$uris"
+
+    curl --request PUT \
+        --url $URL \
+        --header 'Authorization: Bearer'$token \
+        --header 'Content-Type: application/json' > "update$playlist_id.JSON" \
+        --data '{
+        }'
+    #
+}
+
 #program begin here
 filename="shuffleResultPlaylist.txt"
 read -r playlist_id<$filename
 firstLine=false
+auth_token=$(returnToken)
 while read line; do
     if $firstLine ; then
-        echo $line
+        $(updatePlayList $auth_token $playlist_id $line)
     fi
     firstLine=true
 done < $filename
