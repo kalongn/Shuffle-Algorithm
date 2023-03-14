62f1b81fc55361c938c745d2c8bf3d8b3bc21cc4
Ka Long Ngai <kls8162@gmail.com>
2023-03-14 14:28:57 -0400

    basic autho test

---
+++ authoToken.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 8888;
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI;
+
+app.get('/login', (req, res) => {
+    res.redirect('https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}');
+});
