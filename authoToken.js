cce5eda885b470851c921f2be8580f6cec42aee1
Ka Long Ngai <kls8162@gmail.com>
2023-03-14 14:38:41 -0400

    app now request login from user.

--- authoToken.js
+++ authoToken.js
@@ -7,6 +7,11 @@ const CLIENT_ID = process.env.CLIENT_ID;
 const CLIENT_SECRET = process.env.CLIENT_SECRET
 const REDIRECT_URI = process.env.REDIRECT_URI;
 
+
 app.get('/login', (req, res) => {
-    res.redirect('https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}');
+    res.redirect(`https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=code&redirect_uri=${REDIRECT_URI}`);
+});
+
+app.listen(port, () => {
+    console.log(`Express app listening at http://localhost:${port}`);
 });
